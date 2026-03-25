e723f9639c535fdb32038d6a82b7172980d46ffc
Lucas Cavalcante Leandro <162651329+LucasCavalacantee@users.noreply.github.com>
2026-03-25 20:41:57 +0000

    inicio do main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Pessoa{
+    string nome;
+    string cpf;
+    string telefone;
+    string email;
+};
+
+class Trabalhador : Pessoa{
+    
+
+};
+int main(){
+
+
+    return 0;
+}
