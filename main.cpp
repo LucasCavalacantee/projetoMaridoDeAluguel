c656d5d7dd9322d40a8073d0fd313d6c6bb3ac87
Lucas Cavalcante Leandro <162651329+LucasCavalacantee@users.noreply.github.com>
2026-03-27 16:31:58 +0000

    itens modificados

--- main.cpp
+++ main.cpp
@@ -6,12 +6,52 @@ class Pessoa{
     string cpf;
     string telefone;
     string email;
+
+    public:
+    Pessoa(string nome, string cpf, string telefone, string email){
+        this->nome = nome; 
+        this->cpf = cpf;
+        this->telefone = telefone;
+        this->email = email;
+    }
+
 };
 
-class Trabalhador : Pessoa{
-    
+void Pessoa 
+class habilidade{
+    string nomeHabilidade;
+    string descriçãoHabilidade;
+
+    public:
+    habilidade(string nome, string descrição){
+        this->nomeHabilidade = nome;
+        this->descriçãoHabilidade = descrição;
+    }
 
 };
+
+class prestadorServiço : Pessoa{
+    double valorHora;
+
+    public:
+    prestadorServiço(string nome, string cpf, string telefone, string email, double valorHora) : Pessoa(nome, cpf, telefone, email){
+        this->valorHora = valorHora;
+    }
+
+
+};
+
+class cliente : Pessoa{
+    string escolha;
+
+    public:
+    cliente(string nome, string cpf, string telefone, string email, string escolha) : Pessoa(nome, cpf, telefone, email){
+        this->escolha = escolha;
+    }
+
+
+};
+
 int main(){
 
 
